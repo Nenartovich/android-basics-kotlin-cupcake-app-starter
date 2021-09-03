f27899462b6a050707f303925f7ebb4ec2ed7846
Evgeny Nenartovich <nenartovski@yandex.com>
2021-09-03 18:12:45 +0300

    Logic of order creation and price calculation

--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -6,6 +6,9 @@ import androidx.lifecycle.ViewModel
 import java.text.SimpleDateFormat
 import java.util.*
 
+private const val PRICE_PER_CUPCAKE = 2.00
+private const val PRICE_FOR_SAME_DAY_PICKUP = 3.00
+
 class OrderViewModel : ViewModel() {
     private val _quantity: MutableLiveData<Int> = MutableLiveData()
     val quantity: LiveData<Int> get() = _quantity
@@ -21,13 +24,13 @@ class OrderViewModel : ViewModel() {
 
     val dateOptions = getPickupOptions()
 
-
     init {
         resetOrder()
     }
 
     fun setQuantity(numberCupcakes: Int) {
         _quantity.value = numberCupcakes
+        updatePrice()
     }
 
     fun setFlavour(desiredFlavour: String) {
@@ -36,6 +39,7 @@ class OrderViewModel : ViewModel() {
 
     fun setDate(pickupDate: String) {
         _date.value = pickupDate
+        updatePrice()
     }
 
     fun hasNoFlavour() = _flavour.value.isNullOrEmpty()
@@ -54,7 +58,16 @@ class OrderViewModel : ViewModel() {
     fun resetOrder() {
         _quantity.value = 0
         _flavour.value = ""
-        _date.value = dateOptions[0]
+        _date.value = dateOptions[1]
         _price.value = 0.0
+    }
+
+    private fun updatePrice() {
+        var calculatedPrice = (quantity.value ?: 0) * PRICE_PER_CUPCAKE
+        // If the user selected the first option (today) for pickup, add the surcharge
+        if (dateOptions[0] == _date.value) {
+            calculatedPrice += PRICE_FOR_SAME_DAY_PICKUP
+        }
+        _price.value = calculatedPrice
     }
 }
