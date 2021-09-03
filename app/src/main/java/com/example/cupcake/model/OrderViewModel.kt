bea9d18df8c9469b22fbd3f81de706945bd4b4b1
Evgeny Nenartovich <nenartovski@yandex.com>
2021-09-03 17:45:59 +0300

    Bind data

--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -3,19 +3,28 @@ package com.example.cupcake.model
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 class OrderViewModel : ViewModel() {
-    private val _quantity: MutableLiveData<Int> = MutableLiveData(0)
+    private val _quantity: MutableLiveData<Int> = MutableLiveData()
     val quantity: LiveData<Int> get() = _quantity
 
-    private val _flavour: MutableLiveData<String> = MutableLiveData("")
+    private val _flavour: MutableLiveData<String> = MutableLiveData()
     val flavour: LiveData<String> get() = _flavour
 
-    private val _date: MutableLiveData<String> = MutableLiveData("")
+    private val _date: MutableLiveData<String> = MutableLiveData()
     val date: LiveData<String> get() = _date
 
-    private val _price: MutableLiveData<Double> = MutableLiveData(0.0)
+    private val _price: MutableLiveData<Double> = MutableLiveData()
     val price: LiveData<Double> get() = _price
+
+    val dateOptions = getPickupOptions()
+
+
+    init {
+        resetOrder()
+    }
 
     fun setQuantity(numberCupcakes: Int) {
         _quantity.value = numberCupcakes
@@ -27,5 +36,25 @@ class OrderViewModel : ViewModel() {
 
     fun setDate(pickupDate: String) {
         _date.value = pickupDate
+    }
+
+    fun hasNoFlavour() = _flavour.value.isNullOrEmpty()
+
+    private fun getPickupOptions(): List<String> {
+        val options = mutableListOf<String>()
+        val formatter = SimpleDateFormat("EEE, MMM d", Locale.US)
+        val calendar = Calendar.getInstance()
+        repeat(4) {
+            options.add(formatter.format(calendar.time))
+            calendar.add(Calendar.DATE, 1)
+        }
+        return options
+    }
+
+    fun resetOrder() {
+        _quantity.value = 0
+        _flavour.value = ""
+        _date.value = dateOptions[0]
+        _price.value = 0.0
     }
 }
